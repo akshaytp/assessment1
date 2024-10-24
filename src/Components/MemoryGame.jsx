792f7f30804725ff4c8d5c9e7a9e71568ac7f3b4
akshaytp <tpakshay8@gmail.com>
2024-10-24 13:29:03 +0530

    assessment1

--- src/Components/MemoryGame.jsx
+++ src/Components/MemoryGame.jsx
@@ -55,9 +55,9 @@ export default function MemoryGame() {
 
   return (
     <>
-      <div className='flex flex-col items-center bg-gray-100'>
+      <div>
 
-        <div className="grid grid-cols-4 gap-4">
+        <div>
           {cards.map(card => (
             <Card
               key={card.id}
@@ -68,7 +68,6 @@ export default function MemoryGame() {
           ))}
         </div>
         <button
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
           onClick={restartGame}
         >
           Restart Game
